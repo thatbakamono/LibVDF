12f458116928c18b0f9004097a83bf969e0e7eb5
KernelError <villainkernelerror@gmail.com>
2020-02-04 13:45:15 +0100

    Add tests

---
+++ LibVDF.Tests/ParserTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+using System;
+
+namespace LibVDF.Tests
+{
+    public class ParserTests
+    {
+        [Fact]
+        public void TestFieldParsing()
+        {
+            var parser = new VDFParser();
+            var result = parser.ParseContent($"\"name\" \"Kurisu\"{Environment.NewLine}\"surname\" \"Makise\"");
+
+            Assert.Equal("Kurisu", result.GetString("name"));
+            Assert.Equal("Makise", result.GetString("surname"));
+        }
+
+        [Fact]
+        public void TestObjectParsing()
+        {
+            var parser = new VDFParser();
+            var result = parser.ParseContent($"\"info\"{Environment.NewLine}{{{Environment.NewLine}\"name\" \"Kurisu\"{Environment.NewLine}\"surname\" \"Makise\"{Environment.NewLine}}}");
+            var info = result.GetObject("info");
+
+            Assert.Equal("Kurisu", info.GetString("name"));
+            Assert.Equal("Makise", info.GetString("surname"));
+        }
+    }
+}
